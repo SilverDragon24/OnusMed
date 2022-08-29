220a9b23a069fb0a746093e8c03d87e893cc7a44
SilverDragon24 <tarafdar.ion@gmail.com>
2022-08-30 04:34:11 +0530

    updated

--- OnusMed/NewPatient.vb
+++ OnusMed/NewPatient.vb
@@ -19,17 +19,17 @@ Public Class NewPatient
     Private Sub cmbState_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbState.SelectedIndexChanged
         If (cmbState.Text.Length > 0) Then
             cmbState.BackColor = Color.LightGreen
-            valid(4) = Conversions.ToString(True)
+            valid(4) = True
         ElseIf (cmbState.Text.Length = 0) Then
             cmbState.BackColor = Color.Pink
-            valid(4) = Conversions.ToString(False)
+            valid(4) = False
         End If
     End Sub
 
     Private Sub NewPatient_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim num As Integer = 0
         Do
-            valid(num) = Conversions.ToString(False)
+            valid(num) = False
             num = num + 1
         Loop While num <= 6
         txtPatientID.Text = generateID()
@@ -64,33 +64,33 @@ Public Class NewPatient
     Private Sub txtAddress1_TextChanged(sender As Object, e As EventArgs) Handles txtAddress1.TextChanged
         If (txtAddress1.Text.Length > 0) Then
             txtAddress1.BackColor = Color.LightGreen
-            valid(2) = Conversions.ToString(True)
+            valid(2) = True
         ElseIf (txtAddress1.Text.Length = 0) Then
             txtAddress1.BackColor = Color.Pink
-            valid(2) = Conversions.ToString(False)
+            valid(2) = False
         End If
     End Sub
 
     Private Sub txtCity_TextChanged(sender As Object, e As EventArgs) Handles txtCity.TextChanged
         If (txtCity.Text.Length > 0) Then
             txtCity.BackColor = Color.LightGreen
-            valid(3) = Conversions.ToString(True)
+            valid(3) = True
         ElseIf (txtCity.Text.Length = 0) Then
             txtCity.BackColor = Color.Pink
-            valid(3) = Conversions.ToString(False)
+            valid(3) = False
         End If
     End Sub
 
     Private Sub txtEmail_TextChanged(sender As Object, e As EventArgs) Handles txtEmail.TextChanged
         If (txtEmail.Text.Length = 0) Then
-            valid(7) = Conversions.ToString(True)
+            valid(7) = True
             txtEmail.BackColor = Color.LightGreen
         ElseIf (txtEmail.Text.Length > 0) Then
             If (Not (txtEmail.Text.Contains("@") And txtEmail.Text.Contains("."))) Then
-                valid(7) = Conversions.ToString(False)
+                valid(7) = False
                 txtEmail.BackColor = Color.Pink
             Else
-                valid(7) = Conversions.ToString(True)
+                valid(7) = True
                 txtEmail.BackColor = Color.LightGreen
             End If
         End If
@@ -99,40 +99,40 @@ Public Class NewPatient
     Private Sub txtName_TextChanged(sender As Object, e As EventArgs) Handles txtName.TextChanged
         If (txtName.Text.Length > 0) Then
             txtName.BackColor = Color.LightGreen
-            valid(1) = Conversions.ToString(True)
+            valid(1) = True
         ElseIf (txtName.Text.Length = 0) Then
             txtName.BackColor = Color.Pink
-            valid(1) = Conversions.ToString(False)
+            valid(1) = False
         End If
     End Sub
 
     Private Sub txtPatientID_TextChanged(sender As Object, e As EventArgs) Handles txtPatientID.TextChanged
         If (txtPatientID.Text.Length > 0) Then
             txtPatientID.BackColor = Color.LightGreen
-            valid(0) = Conversions.ToString(True)
+            valid(0) = True
         ElseIf (txtPatientID.Text.Length = 0) Then
             txtPatientID.BackColor = Color.Pink
-            valid(0) = Conversions.ToString(False)
+            valid(0) = False
         End If
     End Sub
 
     Private Sub txtPhone_TextChanged(sender As Object, e As EventArgs) Handles txtPhone.TextChanged
         If (txtPhone.Text.Length > 0 And Versioned.IsNumeric(txtPhone.Text)) Then
             txtPhone.BackColor = Color.LightGreen
-            valid(6) = Conversions.ToString(True)
+            valid(6) = True
         ElseIf (txtPhone.Text.Length = 0) Then
             txtPhone.BackColor = Color.Pink
-            valid(6) = Conversions.ToString(False)
+            valid(6) = False
         End If
     End Sub
 
     Private Sub txtPIN_TextChanged(sender As Object, e As EventArgs) Handles txtPIN.TextChanged
         If (txtPIN.Text.Length > 0 And Versioned.IsNumeric(txtPIN.Text)) Then
             txtPIN.BackColor = Color.LightGreen
-            valid(5) = Conversions.ToString(True)
+            valid(5) = True
         ElseIf (txtPIN.Text.Length = 0 Or Not Versioned.IsNumeric(txtPIN.Text)) Then
             txtPIN.BackColor = Color.Pink
-            valid(5) = Conversions.ToString(False)
+            valid(5) = False
         End If
     End Sub
 End Class
